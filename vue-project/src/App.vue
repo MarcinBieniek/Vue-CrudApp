1c3b15b824f9f81e1cbab3e5d39228446702fb2b
Marcin Bieniek <marcingim@gmail.com>
2023-05-26 16:57:46 +0200

    Add basic structure with array rendering

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,79 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+  import { ref, reactive } from 'vue';
+
+  const collection = reactive([
+    {
+      id: 1,
+      author: "J.R.R. Tolkien",
+      title: "Lord of the Rings"
+    },
+    {
+      id: 2,
+      author: "J.K. Rowling",
+      title: "Harry Potter"
+    },
+    {
+      id: 3,
+      author: "Andrzej Sapkowski",
+      title: "The Witcher"
+    }
+  ])
+  const wishlist = reactive([])
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1 class="header">My bookshelft</h1>
+  <div class="section">
+    <div class="left">
+      <div class="collection">
+        <h2>Books in collection</h2>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <ul class="books">
+          <li v-for="item in collection" :key="item.id" class="book">
+            <p class="number">{{ item.id }}</p>
+            <p class="author">{{ item.author }}</p>
+            <p class="title">{{ item.title }}</p>
+          </li>
+        </ul>
+
+      </div>
+      <div class="wishlist">
+        <h2>My wishlist</h2>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="right">asd</div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+  .header {
+    text-align: center;
+    padding: 20px 0px;
+  }
 
-@media (min-width: 1024px) {
-  header {
+  .section {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  .left {
+    flex: 1;
+    border: 1px solid grey;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .collection {
+    padding: 20px;
   }
-}
+
+  .wishlist {
+    padding: 20px;
+  }
+
+  .right {
+    flex: 1;
+    border: 1px solid grey;
+  }
+
 </style>
